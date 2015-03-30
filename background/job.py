511cacb2d9dd65854026105ca542513484573116
Jason <jxieeducation@berkeley.edu>
2015-03-29 21:49:55 -0700

    sitemap daily generation

--- background/job.py
+++ background/job.py
@@ -1,9 +1,12 @@
+#note run this script from the thrashing base directory
 import schedule
 import time
 import datetime
 from subprocess import call
 import boto
 import json
+from creepy import Crawler
+import sys
 
 AWS_key = ""
 AWS_secret = ""
@@ -33,8 +36,41 @@ def backup():
 	k.key = datetime.datetime.now().strftime("%y/%m/%d/backup.zip")
 	k.set_contents_from_filename(db_file, cb=percent_cb, num_cb=10)
 
+def generate_sitemap():
+    siteurl = ""
+    class SiteCrawler(Crawler):
+        urls = []
+        def process_document(self, doc):
+            if doc.status == 200:
+                if siteurl in doc.url:
+                    self.urls += [doc.url]
+            else:
+                pass
+    def getUrls (url):
+        crawler = SiteCrawler()
+        crawler.set_follow_mode(Crawler.F_SAME_HOST)
+        crawler.add_url_filter('\.(jpg|jpeg|gif|png|js|css|swf)$')
+        siteurl = url
+        crawler.crawl(url)
+        return crawler.urls
+
+    intro = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n'
+    end = '</urlset>'
+    body = ''
+    urls = getUrls("http://www.thrashing.io")
+    for url in urls:
+        body += '<url>\n'
+        body += '\t<loc>' + url + '</loc>\n'
+        body += '</url>\n'
+    output = intro + body + end
+    out = 'public/sitemap.xml'
+    f = open(out,'w')
+    f.write(output)
+    f.close()
+
 # note 11:00 UTC is 3:00 in Pacific time 
 schedule.every().day.at("11:00").do(backup)
+schedule.every().day.at("11:00").do(generate_sitemap)
 
 while True:
     schedule.run_pending()
