8b157abfcbafc3b58e96c11837724cedc869df37
Jason <jxieeducation@berkeley.edu>
2015-04-06 15:12:04 -0700

    added deploy script changes

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get('/archive', function (req, res){
     });
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
 	var host = server.address().address
 	var port = server.address().port
 	console.log('listening at http://%s:%s', host, port)
--- deploy.sh
+++ deploy.sh
@@ -2,46 +2,40 @@
 
 #elastic search
 sudo apt-get update
-#curl http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.1.zip -o temp-es.zip && unzip temp-es.zip && rm temp-es.zip && mv elasticsearch-0.90.1 ~/bin/elasticsearch && ~/bin/elasticsearch/bin/elasticsearch
 #https://www.digitalocean.com/community/tutorials/how-to-install-elasticsearch-on-an-ubuntu-vps
-sudo apt-get install unzip zip
+sudo apt-get install unzip zip openjdk-6-jre python-pip
 mkdir bin
 curl http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.1.zip -o temp-es.zip && unzip temp-es.zip && rm temp-es.zip && mv elasticsearch-0.90.1 ~/bin/elasticsearch && ~/bin/elasticsearch/bin/elasticsearch
 
 #node stack
-sudo apt-get install git
-sudo apt-get install mongodb
+sudo apt-get install git mongodb
 # http://stackoverflow.com/questions/27703218/mean-stack-on-ubuntu-14-04-suddenly-stopped-working 
-<-- installs node and npm
+#installs node and npm
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs 
+node -v
+
 git clone https://github.com/jxieeducation/thrashing
 cd thrashing
 mkdir databasefile
 npm i
+# fix mongoose shit: http://stackoverflow.com/questions/28651028/cannot-find-module-build-release-bson-code-module-not-found-js-bson
 
 #python scheduler
-sudo apt-get install python-pip
 sudo pip install schedule boto creepy
-
-#mongodb screen
-screen -S mongo
-cd databasefile
-mongod --dbpath . --smallfiles
 
 # scheduler screen
 screen -S scheduler
-scp -i /worker.pem private_settings.json ubuntu@ec2-54-69-180-226.us-west-2.compute.amazonaws.com:~/thrashing/
+scp -i /worker.pem private_settings.json ubuntu@ec2-54-69-49-75.us-west-2.compute.amazonaws.com:~/thrashing/
 python background/job.py
-
-#indexing screen
-screen -S indexing
-nodejs background/indexDatabase.js
 
 # screen for main app
 screen -S app
---> change ports from 3000 to the real one
-
+sudo node app.js
 
 #to load db from backup dump
+scp -i /worker.pem ~/Downloads/backup.zip ubuntu@ec2-54-69-49-75.us-west-2.compute.amazonaws.com:~/
+# migrations stuff
 mongorestore .....pathtodump.....
 
 
